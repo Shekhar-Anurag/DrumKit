c8a069baf2c2f3191c799ff48870e88b37750834
Shekhar-Anurag <97689657+Shekhar-Anurag@users.noreply.github.com>
2024-01-23 19:02:06 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+
+
+const wAudio = new Audio("./sounds/tom-1.mp3");
+const aAudio = new Audio("./sounds/tom-2.mp3");
+const sAudio = new Audio("./sounds/tom-3.mp3");
+const dAudio = new Audio("./sounds/tom-4.mp3");
+const jAudio = new Audio("./sounds/kick-bass.mp3");
+const kAudio = new Audio("./sounds/crash.mp3");
+const lAudio = new Audio("./sounds/snare.mp3");
+
+var len = document.querySelectorAll(".drum").length;
+
+for(var i =0;i<len;i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        playAudio(this.innerHTML)
+        
+    });
+}
+
+document.addEventListener("keypress",function(event){
+    playAudio(event.key)
+});
+
+function playAudio(key){
+    switch(key){
+        case "w": wAudio.currentTime = 0; wAudio.play();
+        break;
+
+        case "a": aAudio.currentTime = 0; aAudio.play();
+        break;
+
+        case "s": sAudio.currentTime = 0; sAudio.play();
+        break;
+
+        case "d": dAudio.currentTime = 0; dAudio.play();
+        break;
+
+        case "j": jAudio.currentTime = 0; jAudio.play();
+        break;
+
+        case "k": kAudio.currentTime = 0; kAudio.play();
+        break;
+
+        case "l": lAudio.currentTime = 0; lAudio.play();
+        break;
+
+        default : console.log(this.innerHTML);
+    }
+}
+
+
+
+
+
